394a592cfbabe736ee736f57805473d651d94394
Steven Abreu <stevenabreu7@gmail.com>
2018-03-23 20:46:08 +0100

    + implemented instruction classes

--- Instruction_move.cpp
+++ Instruction_move.cpp
@@ -1,9 +1,29 @@
 #include "Instruction_move.h"
 
 void Instruction_move::parse(std::string instr) {
-    x = tstate();
+    /* move x y */
+    std::string delimiter = " ";
+    size_t pos = 0;
+    std::string token;
+    int i = 0;
+    
+    while ((pos = instr.find(delimiter)) != std::string::npos) {
+        token = instr.substr(0, pos);
+        switch (i) {
+            case 0:
+                break;
+            case 1:
+                x = tstate(std::stoi(token));
+                break;
+            case 2:
+                y = tstate(std::stoi(token));
+                break;
+        }
+        instr.erase(0, pos + delimiter.length());
+        i++;
+    }
 }
 
 void Instruction_move::execute(Bug b) {
-    x = tstate();
+    b.set_position(tposition(x,y));
 }
--- Instruction_pickup.cpp
+++ Instruction_pickup.cpp
@@ -1,9 +1,29 @@
 #include "Instruction_pickup.h"
 
 void Instruction_pickup::parse(std::string instr) {
-    x = tstate();
+    /* pickup x y */
+    std::string delimiter = " ";
+    size_t pos = 0;
+    std::string token;
+    int i = 0;
+    
+    while ((pos = instr.find(delimiter)) != std::string::npos) {
+        token = instr.substr(0, pos);
+        switch (i) {
+            case 0:
+                break;
+            case 1:
+                x = tstate(std::stoi(token));
+                break;
+            case 2:
+                y = tstate(std::stoi(token));
+                break;
+        }
+        instr.erase(0, pos + delimiter.length());
+        i++;
+    }
 }
 
 void Instruction_pickup::execute(Bug b) {
-    x = tstate();
+    b.get_food();
 }
